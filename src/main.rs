2823909dc83d0970a7867d2d3bd4cf5afff940a1
Murillo Ramos <murillo.tech.dev@gmail.com>
2026-04-08 16:32:38 -0300

    adding a "for" loop alternative

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+//     let mut i = 0;
+
+//     while i < 5 {
+//         println!("the value is: {}", a[i]);
+
+//         i += 1;
+//     }
+// }
+
+// This approach is **error-prone**; the code would **panic** if we change the definition of the `a` array for having **four elements** instead of **five** but forgetting to update the condition to `while index < 4`.
+
+// A better alternative is using a `for` loop and execute some code for each item in a collection (*array*):
+
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut i = 0;
 
-    while i < 5 {
-        println!("the value is: {}", a[i]);
-
-        i += 1;
+    for element in a {
+        println!("the value is: {element}");
     }
 }
