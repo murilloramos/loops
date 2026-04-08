669c29d19d88768507a536f26cc8598394af36f4
Murillo Ramos <murillo.tech.dev@gmail.com>
2026-04-08 15:02:25 -0300

    looping through a collection with the "for" keyword

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 fn main() {
-    let mut number = 3;
+    let a = [10, 20, 30, 40, 50];
+    let mut i = 0;
 
-    while number != 0 {
-        println!("{number}!");
+    while i < 5 {
+        println!("the value is: {}", a[i]);
 
-        number -= 1;
+        i += 1;
     }
-
-    println!("LIFTOFF!!!");
 }
